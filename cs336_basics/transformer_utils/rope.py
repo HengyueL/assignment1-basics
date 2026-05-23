1fc800f056ec155a284a9d3f2425115af675ed1b
屁屁啵的大烧饼 <pipibo@pipibodedashaobingdeMacBook-Air.local>
2026-05-22 20:32:51 -0700

    Implement Rope

--- cs336_basics/transformer_utils/rope.py
+++ cs336_basics/transformer_utils/rope.py
@@ -10,16 +10,33 @@ class RoPE(nn.Module):
         d_k: int,
         max_seq_len: int,
         device: torch.device | None = None,
-    ):
+    ): 
         super().__init__()
-        raise NotImplementedError
+        k = torch.arange(0, d_k, 2, dtype=torch.float32, device=device)
+        freq = 1. / (theta ** (k / d_k))
+        positions = torch.arange(max_seq_len, device=device)
+        angles = torch.outer(positions, freq)
+        
+        cos = torch.cos(angles)
+        sin = torch.sin(angles)
+        self.register_buffer("cache_cos", cos, persistent=False)
+        self.register_buffer("cache_sin", sin, persistent=False)
 
     def forward(
         self,
         x: torch.Tensor,
         token_positions: torch.Tensor,
     ) -> torch.Tensor:
-        raise NotImplementedError
+        cos_values = self.cache_cos[token_positions]
+        sin_values = self.cache_sin[token_positions]
+        x1, x2 = x[..., ::2], x[..., 1::2]
+
+        rotated_x1 = cos_values * x1 - sin_values * x2
+        rotated_x2 = sin_values * x1 + cos_values * x2
+        return rearrange(
+            torch.stack([rotated_x1, rotated_x2], dim=-1), 
+            '... d_half two -> ... (d_half two)'
+        )
 
 
 if __name__ == "__main__":
@@ -28,4 +45,3 @@ if __name__ == "__main__":
         d_k=4,
         max_seq_len=10
     )
-    print()
--- tests/adapters.py
+++ tests/adapters.py
@@ -229,7 +229,13 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    from cs336_basics.transformer_utils.rope import RoPE
+    rope = RoPE(
+        theta=theta,
+        d_k=d_k,
+        max_seq_len=max_seq_len,
+    )
+    return rope(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
